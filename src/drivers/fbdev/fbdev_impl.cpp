5f45e20ac7686b67e9cc45380719a938c1386195
nt4t <nt4t@users.noreply.github.com>
2026-06-03 18:37:12 +0700

    add F3 key to toggle FPS display on fbdev

--- src/drivers/fbdev/fbdev_impl.cpp
+++ src/drivers/fbdev/fbdev_impl.cpp
@@ -84,6 +84,10 @@ bool fbdev_impl::process_events() {
             } else if (pressed && keycode == KEY_ESC) {
                 fb_input->clear_stored_events();
                 return true;
+            } else if (pressed && keycode == KEY_F3) {
+                auto *fb_video = static_cast<fbdev_video*>(video.get());
+                fb_video->set_fps_enabled(!fb_video->get_fps_enabled());
+                LOG(INFO, "FPS display: {}", fb_video->get_fps_enabled() ? "on" : "off");
             }
             
             uint16_t sdlk = fb_input->keycode_to_sdlk(keycode);
