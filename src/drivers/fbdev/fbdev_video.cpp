8105bcb6fe7182a91ef62b531734d2a212a08d9a
nt4t <nt4t@users.noreply.github.com>
2026-06-03 20:34:42 +0700

    fix FPS text coords: initialize t/b in get_text_width_and_height

--- src/drivers/fbdev/fbdev_video.cpp
+++ src/drivers/fbdev/fbdev_video.cpp
@@ -149,7 +149,7 @@ void fbdev_video::render(const void *data, int width, int height, size_t pitch) 
             char fps_text[32];
             int len = snprintf(fps_text, sizeof(fps_text), "FPS: %.1f", current_fps);
             if (len > 0) {
-                int tw, tt, tb;
+                int tw = 0, tt = 255, tb = -255;
                 get_text_width_and_height(fps_text, tw, tt, tb);
                 set_draw_color(0, 0, 0, 255);
                 fill_rectangle(8, 8 + tt, tw + 4, tb - tt + 4);
