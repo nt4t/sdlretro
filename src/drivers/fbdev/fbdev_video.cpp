9f818579c106f0a3852ca55103a5b5915a601cc9
nt4t <nt4t@users.noreply.github.com>
2026-06-03 20:28:20 +0700

    clear rectangle under FPS text before drawing

--- src/drivers/fbdev/fbdev_video.cpp
+++ src/drivers/fbdev/fbdev_video.cpp
@@ -149,6 +149,10 @@ void fbdev_video::render(const void *data, int width, int height, size_t pitch) 
             char fps_text[32];
             int len = snprintf(fps_text, sizeof(fps_text), "FPS: %.1f", current_fps);
             if (len > 0) {
+                int tw, tt, tb;
+                get_text_width_and_height(fps_text, tw, tt, tb);
+                set_draw_color(0, 0, 0, 255);
+                fill_rectangle(8, 8 + tt, tw + 4, tb - tt + 4);
                 set_draw_color(255, 255, 255, 255);
                 draw_text(10, 10, fps_text, 0, true);
             }
