0e46f79df033ec9c54e0ae59bfa9183ac19089f2
nt4t <nt4t@users.noreply.github.com>
2026-06-04 12:21:45 +0700

    add get_font_size override: return 9px for fbdev

--- src/drivers/fbdev/include/fbdev_video.h
+++ src/drivers/fbdev/include/fbdev_video.h
@@ -37,6 +37,7 @@ public:
     bool get_fps_enabled() const { return fps_enabled; }
 
     void set_draw_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) override;
+    int get_font_size() const override { return 9; }
 
 private:
     void draw_text_impl(int x, int y, const char *text, int width, bool shadow);
