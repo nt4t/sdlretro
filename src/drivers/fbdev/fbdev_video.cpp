69b967db3893f7be21734c34c774525b1cbe22ea
nt4t <nt4t@users.noreply.github.com>
2026-06-05 17:51:25 +0700

    Replace WWW background with fill_rectangle for FPS overlay

--- src/drivers/fbdev/fbdev_video.cpp
+++ src/drivers/fbdev/fbdev_video.cpp
@@ -165,18 +165,8 @@ void fbdev_video::render(const void *data, int width, int height, size_t pitch) 
                     tw += fd.sw;
                 }
                 
-                char clear_text[64] = {0};
-                int clear_w = 0;
-                int i = 0;
-                while (clear_w < tw + 8 && i < 63) {
-                    clear_text[i++] = 'W';
-                    int idx = 87;
-                    const font_data_t &fd = get_pixel_font_data(idx);
-                    clear_w += fd.sw;
-                }
-                
-              set_draw_color(0, 0, 0, 255);
-                draw_text(8, 8, clear_text, 0, false);
+                set_draw_color(0, 0, 0, 0);
+                fill_rectangle(6, 6, tw + 8, 11);
                 set_draw_color(255, 255, 255, 255);
                 draw_text(10, 10, fps_text, 0, false);
             }
