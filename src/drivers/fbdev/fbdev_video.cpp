6c900db39b7b5a5ac176661a0911f087f1b7078c
nt4t <nt4t@users.noreply.github.com>
2026-06-06 08:42:04 +0700

    fbdev: Fix EXPAND_16_TO_16 buffer cast in 16->16 parallel path

--- src/drivers/fbdev/fbdev_video.cpp
+++ src/drivers/fbdev/fbdev_video.cpp
@@ -1076,8 +1076,8 @@ void fbdev_video::render_scaled_parallel(const void *data, int width, int height
                         EXPAND_16_TO_16(j.h_line_16_buf, reinterpret_cast<uint16_t*>(j.h_line_buf), width, scale);
                         memcpy(dest_row_16, reinterpret_cast<uint16_t*>(j.h_line_buf), scaled_w * sizeof(uint16_t));
                     } else {
-                        EXPAND_16_TO_16(src_row_16, j.h_line_buf, width, scale);
-                        memcpy(dest_row_16, j.h_line_buf, scaled_w * sizeof(uint16_t));
+                        EXPAND_16_TO_16(src_row_16, reinterpret_cast<uint16_t*>(j.h_line_buf), width, scale);
+                        memcpy(dest_row_16, reinterpret_cast<uint16_t*>(j.h_line_buf), scaled_w * sizeof(uint16_t));
                     }
                 }
             }
